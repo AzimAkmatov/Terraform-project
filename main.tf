6d31da99c00ea43472a9a36ada28d52a30c3052a
Azim <azim.akmatov@gmail.com>
2024-03-13 09:25:08 -0500

    vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
     region = "us-east-2"  
 }
+resource "aws_vpc" "test" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-resource "aws_instance" "foo" {
+  tags = {
+    Name = "test"
+  }
+}
+resource "aws_instance" "test" {
   ami           = "ami-022661f8a4a1b91cf"
   instance_type = "t2.micro"
   tags = {
